84bc071967065fb39fdd7ade7f7ea4cc556d8ca4
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-09-01 05:37:42 -0400

    (Re)Added InputDisabler

---
+++ InputDisabler/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+#Fix InputDisabler to support all the devices to reduce duplication.
+ifneq ($(filter gprimeltecan gprimeltetmo gprimeltemtr, $(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := InputDisabler
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PACKAGE)
+endif
