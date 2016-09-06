ebc5b3bd62b705bfc4522ee84e24e1a6fcb2cde2
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-09-06 04:54:16 -0400

    Removed release tools

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -7,6 +7,9 @@ LOCAL_PATH := device/samsung/gprimelte-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
+# Releasetools
+#TARGET_RELEASETOOLS_EXTENSIONS                  := $(LOCAL_PATH)
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
