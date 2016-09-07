892d95e1a35349ee4c0bf1ecb43e6b59d650c952
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-09-07 19:06:20 -0400

    InputDisabler: Updated Constants class

--- InputDisabler/src/org/davis/inputdisabler/Constants.java
+++ InputDisabler/src/org/davis/inputdisabler/Constants.java
@@ -18,16 +18,19 @@ public final class Constants {
 		"android.intent.action.DOZE_PULSE_STARTING";
 
 	/* Declare some paths to the touch screen sysfs interfaces*/
-	private static final String TS_PATH_1 =
+	private final String TS_PATH_1 =
 		"/sys/devices/soc.0/i2c.78/i2c-11/11-0020/input/input1/enabled";
-	private static final String TS_PATH_2 =
+	private final String TS_PATH_2 =
 		"/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0020/input/input2/enabled";
-	private static final String TS_PATH_3 =
+	private final String TS_PATH_3 =
 		"/sys/devices/soc.0/i2c.72/i2c-12/12-0048/input/input3/enabled";
+	private final String TS_PATH_4 =
+		"/sys/class/input/input3/enabled";
 
 	/* Declare some File objects */
-	private static File ts_1 = new File(TS_PATH_1);
-	private static File ts_2 = new File(TS_PATH_2);
+	private File ts_1 = new File(TS_PATH_1);
+	private File ts_2 = new File(TS_PATH_2);
+	private File ts_3 = new File(TS_PATH_3);
 
 	/* Define a getter for the touch screen path */
 	public static String getTsPath() {
@@ -39,8 +42,11 @@ public final class Constants {
 		else if (ts_2.exists()) {
 			return this.TS_PATH_2;
 		}
-		//else:
-    	return this.TS_PATH_3;
+		else if (ts_3.exists()) {
+			return this.TS_PATH_3;
+		}
+		/* in case none of the others are extant, try the generic path as backup */
+    	return this.TS_PATH_4;
     }
 
 }
