3ba8261bcee0a63b32e54eafbb0562fbbdb9a84a
Zvikomborero VIncent Zvikaramba <zvikovincent@gmail.com>
2016-09-19 09:44:20 -0400

    include: Updated cryptfs

--- include/cryptfs_hw.h
+++ include/cryptfs_hw.h
@@ -34,11 +34,13 @@ extern "C" {
 #endif
 
 unsigned int wipe_hw_device_encryption_key(const char*);
+int set_hw_device_encryption_key(const char*, const char*);
+int update_hw_device_encryption_key(const char*, const char*, const char*);
+int clear_hw_device_encryption_key(void);
 unsigned int is_hw_disk_encryption(const char*);
 unsigned int is_hw_fde_enabled(void);
-int set_hw_device_encryption_key(const char*, const char*);
-int update_hw_device_encryption_key(const char*, const char*);
 int is_ice_enabled(void);
+int should_use_keymaster();
 
 #ifdef __cplusplus
 }
