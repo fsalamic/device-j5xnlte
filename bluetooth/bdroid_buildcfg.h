905cb504a58d06fefb2b3314b4d2d0f6ce99b3b3
Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
2017-01-12 02:21:10 +0000

    bluetooth: Update bluedroid config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,11 +17,8 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy Grand Prime"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Grand Prime"
 #define BLUETOOTH_QTI_SW TRUE
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
 #define BTC_INCLUDED TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
