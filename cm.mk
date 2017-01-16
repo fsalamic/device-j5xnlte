c88291694cbdb8bb0852e3ffd5f95477aa656027
Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
2017-01-16 20:30:45 +0000

    Add common cm.mk

---
+++ cm.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
+CM_BUILDTYPE := NIGHTLY
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
