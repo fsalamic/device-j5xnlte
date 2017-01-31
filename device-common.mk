aa15e4a59f6d40cc68a389a5373136e3d0e975c9
Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
2017-01-31 04:51:59 +0000

    gprimelte: Decomonise nfc files

--- device-common.mk
+++ device-common.mk
@@ -27,13 +27,11 @@ PRODUCT_PACKAGES += \
 	Tag \
 	com.android.nfc_extras \
 	com.android.nfc.helper \
-	NfcNci \
-	libnfc-nci
+	libnfc_nci_jni \
+	libnfc_jni
 
 # NFC prebuilt files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-	$(LOCAL_PATH)/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
 	$(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
 	packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt
 
