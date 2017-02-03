3f64e7b2e7312192495031bc64ab83290d117450
deadman96385 <seanhoyt963@gmail.com>
2017-02-02 23:25:53 -0600

    Inherit from msm8916-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,5 @@
-FORCE_32_BIT := true
+# Inherit from common
+-include device/samsung/msm8916-common/BoardConfigCommon.mk
 
 LOCAL_PATH := device/samsung/gprimelte-common
 
@@ -7,127 +8,25 @@ LOCAL_PATH := device/samsung/gprimelte-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
-# Releasetools
-#TARGET_RELEASETOOLS_EXTENSIONS                  := $(LOCAL_PATH)
-
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
 # Platform
-TARGET_ARCH := arm
+BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := msm8916
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_BOARD_PLATFORM_GPU       := qcom-adreno306
-#ARCH_ARM_HAVE_TLS_REGISTER := true
-
-# Architecture
-TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-# Audio
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-BOARD_USES_ALSA_AUDIO := true
-USE_LEGACY_AUDIO_POLICY := 0
-USE_CUSTOM_AUDIO_POLICY := 1
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_USES_QCOM_MM_AUDIO := true
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-
 # Custom RIL class
 BOARD_RIL_CLASS    := ../../../device/samsung/gprimelte-common/ril
 PROTOBUF_SUPPORTED := true
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-
-# Camera
-TARGET_USE_VENDOR_CAMERA_EXT := true
-TARGET_PROVIDES_CAMERA_HAL := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-
 # Charger
-BOARD_CHARGER_ENABLE_SUSPEND    := true
-BOARD_CHARGER_SHOW_PERCENTAGE   := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH           := /sys/class/power_supply/battery/batt_lp_charging
-
-# Enable QCOM FM feature
-AUDIO_FEATURE_ENABLED_FM := true
-BOARD_HAVE_QCOM_FM := true
-
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS :=	$(LOCAL_PATH)/cmhw
-BOARD_HARDWARE_CLASS +=	\
-	hardware/cyanogen/cmhw \
-	hardware/samsung/cmhw
 
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-# default.prop
-ADDITIONAL_DEFAULT_PROPERTIES += \
-	camera2.portability.force_api=1
-
-# Display
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-TARGET_HAVE_NEW_GRALLOC := true
-
-# Encryption
-TARGET_SWV8_DISK_ENCRYPTION := true
-
-# FM
-AUDIO_FEATURE_ENABLED_FM := true
-TARGET_QCOM_NO_FM_FIRMWARE := true
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# GPS
-#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm8916
-#TARGET_NO_RPC := true
-
-# Healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
-
-#ART
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-
-# Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-TARGET_UNIFIED_DEVICE := true
-TARGET_PROVIDES_INIT_RC := false
-
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
@@ -135,9 +34,6 @@ TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := false
-
-# malloc implementation
-MALLOC_IMPL := dlmalloc
 
 # Partition sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE      := 13631488
@@ -148,86 +44,13 @@ BOARD_FLASH_BLOCK_SIZE              := 131072
 #i think this should be 4096
 #blockdev --getbsz /dev/block/mmcblk0p9
 
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-CM_POWERHAL_EXTENSION := qcom
-WITH_QC_PERF := true
-
-# Media
-TARGET_QCOM_MEDIA_VARIANT           := caf
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
-# Qualcomm support
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
-HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE   := true
-USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY := true
-TARGET_USES_NEW_ION_API := true
-
-#Includes
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
 # RIL
 TARGET_RIL_VARIANT := caf
 #override to enable audio.
 BOARD_PROVIDES_LIBRIL := false
 
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/sepolicy.mk
-include vendor/cm/sepolicy/qcom/sepolicy.mk
-
 BOARD_SEPOLICY_DIRS += \
     device/samsung/gprimelte-common/sepolicy
 
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/gprimelte-common/recovery/fstab.qcom
-BOARD_HAS_LARGE_FILESYSTEM			:= true
-TARGET_RECOVERY_DENSITY 			:= hdpi
-BOARD_HAS_NO_MISC_PARTITION 		:= true
-BOARD_HAS_NO_SELECT_BUTTON 			:= true
-BOARD_RECOVERY_SWIPE 				:= true
-BOARD_USE_CUSTOM_RECOVERY_FONT 	    := \"roboto_23x41.h\"
-BOARD_USES_MMCUTILS 				:= true
-BOARD_SUPPRESS_EMMC_WIPE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := false
-
-# Vold
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-BOARD_VOLD_MAX_PARTITIONS := 67
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
-# Wifi
-WLAN_CHIPSET := pronto
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
-BOARD_HAVE_SAMSUNG_WIFI := true
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-TARGET_USES_QCOM_WCNSS_QMI := true
-TARGET_USES_WCNSS_CTRL := true
-WIFI_DRIVER_FW_PATH_AP := "ap"
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH  := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
-
-#make, move, symlink and strip the wlan kernel module.
-KERNEL_EXTERNAL_MODULES:
-	+$(MAKE) -C device/samsung/gprimelte-common/wlan/prima/ WLAN_ROOT=$(ANDROID_BUILD_TOP)/device/samsung/gprimelte-common/wlan/prima/ \
-		KERNEL_SOURCE=$(KERNEL_OUT) ARCH="arm" \
-		CROSS_COMPILE="arm-eabi-"
-	mkdir $(KERNEL_MODULES_OUT)/$(WLAN_CHIPSET)/ -p
-	ln -sf /system/lib/modules/$(WLAN_CHIPSET)/$(WLAN_CHIPSET)_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
-	mv device/samsung/gprimelte-common/wlan/prima/wlan.ko $(KERNEL_MODULES_OUT)/$(WLAN_CHIPSET)/$(WLAN_CHIPSET)_wlan.ko
-	arm-eabi-strip --strip-debug $(KERNEL_MODULES_OUT)/$(WLAN_CHIPSET)/$(WLAN_CHIPSET)_wlan.ko
-	+$(MAKE) -C device/samsung/gprimelte-common/wlan/prima/ clean
-TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
--- device-common.mk
+++ device-common.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
+$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 include $(LOCAL_PATH)/keylayout/Layouts.mk
 
@@ -9,18 +9,6 @@ LOCAL_PATH := device/samsung/gprimelte-common
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-#Android EGL implementation
-PRODUCT_PACKAGES += libGLES_android
-
-PRODUCT_PACKAGES += libtime_genoff
-
-# ANT+
-PRODUCT_PACKAGES += \
-	AntHalService \
-	antradio_app \
-	com.dsi.ant.antradio_library \
-	libantradio
 
 #NFC
 PRODUCT_PACKAGES += \
@@ -41,33 +29,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/setNFCperms:system/etc/init.d/setNFCperms
 
-#JARS
-PRODUCT_PACKAGES += \
-	com.google.widevine.software.drm
-
 # Audio
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	audio.primary.msm8916 \
-	audio.primary.default \
-	audio_policy.msm8916 \
-	audio.r_submix.default \
-	audio.usb.default \
-	audiod \
-	libaudio-resampler \
-	libaudioroute \
-	libaudioutils \
-	libaudiopolicymanager \
-	libqcompostprocbundle \
-	libqcomvisualizer \
-	libqcomvoiceprocessing \
-	libqcmediaplayer \
-	libtinyalsa \
-	tinymix \
-	tinyplay \
-	tinycap \
-	libFLAC \
-	tinypcminfo
+	libFLAC
 
 #VoLTE calling support
 PRODUCT_PACKAGES += \
@@ -76,38 +40,10 @@ PRODUCT_PACKAGES += \
 	ims-common \
 	voip-common
 
-# Charger
-PRODUCT_PACKAGES += \
-	cm-charger_res_images
-
-# Boot jars
-PRODUCT_BOOT_JARS += \
-	tcmiface
-
-# BoringSSL Hacks
-PRODUCT_PACKAGES += \
-	libboringssl-compat
-
 # SHIM RIL
 PRODUCT_PACKAGES += \
 	libril_shim
 
-# Connectivity Engine support
-PRODUCT_PACKAGES += \
-	libcnefeatureconfig
-
-#Location, WiDi
-PRODUCT_PACKAGES += \
-	com.android.location.provider \
-	com.android.location.provider.xml \
-	com.android.media.remotedisplay \
-	com.android.media.remotedisplay.xml
-
-#Camera
-PRODUCT_PACKAGES += \
-	libmm-qcamera \
-	camera.msm8916
-	
 # Doze
 PRODUCT_PACKAGES += \
 	SamsungDoze
@@ -115,21 +51,6 @@ PRODUCT_PACKAGES += \
 # Touch issue workaround
 PRODUCT_PACKAGES += \
 	InputDisabler
-	
-# Display
-PRODUCT_PACKAGES += \
-	copybit.msm8916 \
-	gralloc.msm8916 \
-	hwcomposer.msm8916 \
-	libtinyxml \
-	libtinyxml2 \
-	memtrack.msm8916
-
-# Ebtables
-PRODUCT_PACKAGES += \
-	ebtables \
-	ethertypes \
-	libebtc
 
 # FM
 PRODUCT_PACKAGES += \
@@ -175,199 +96,54 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
 	$(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
-# Keystore
-PRODUCT_PACKAGES += \
-	keystore.msm8916
-
-# libhealthd.qcom
-PRODUCT_PACKAGES += \
-	libhealthd.qcom \
-	libhealthd
-
-# Lights
-PRODUCT_PACKAGES += \
-	lights.msm8916
-
 # Default Property Overrides
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp \
 	persist.eons.enabled=true \
-	persist.radio.apm_sim_not_pwdn=1 \
 	rild.libpath=/system/lib/libsec-ril.so \
-	persist.cne.feature=0 \
 	persist.radio.lte_vrte_ltd=1 \
 	persist.radio.add_power_save=1 \
 	camera2.portability.force_api=1 \
-	ro.secure=0 \
-	ro.adb.secure=0 \
-	ro.debuggable=1 \
-	persist.service.adb.enable=1
-
-#Sensors
-PRODUCT_PACKAGES += \
-	sensors.default
-
-#GPS Configurations
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-	$(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-	$(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-	$(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
-# macloader
-PRODUCT_PACKAGES += macloader
+	ro.secure=0
 
 # Audio configurations
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
 	$(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf
 
-# Media configurations
-PRODUCT_COPY_FILES += \
-	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-
 # Configuration files
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf \
 	$(LOCAL_PATH)/configs/Diag.cfg:system/etc/Diag.cfg \
 	$(LOCAL_PATH)/configs/Diag_audio.cfg:system/etc/Diag_audio.cfg \
 	$(LOCAL_PATH)/configs/Diag_volte.cfg:system/etc/Diag_volte.cfg \
-	$(LOCAL_PATH)/configs/Diag_zero.cfg:system/etc/Diag_zero.cfg \
-	$(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
-	$(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
-	$(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-	$(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml \
-
-# Snap Camera
-PRODUCT_PACKAGES += Snap
-
-# Misc
-PRODUCT_PACKAGES += \
-	busybox \
-	curl \
-	libbson \
-	libcurl \
-	javax.btobex \
-	tcpdump \
-	libkeyutils \
-	libjpega \
-	libexifa \
-	libstlport \
-	datatop \
-	sockev \
-	librmnetctl
-
-# OMX
-PRODUCT_PACKAGES += \
-	libdashplayer \
-	libdivxdrmdecrypt \
-	libmm-omxcore \
-	libOmxAacEnc \
-	libOmxAmrEnc \
-	libOmxCore \
-	libOmxEvrcEnc \
-	libOmxQcelp13Enc \
-	libOmxSwVencMpeg4 \
-	libOmxVdec \
-	libOmxVdecHevc \
-	libOmxVenc \
-	libOmxVidEnc \
-	libOmxVdpp \
-	libstagefrighthw
+	$(LOCAL_PATH)/configs/Diag_zero.cfg:system/etc/Diag_zero.cfg
 
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
 	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
 	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
 	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml \
-	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
-
-# Power HAL
-PRODUCT_PACKAGES += \
-	power.qcom
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.use_data_netmgrd=false \
-	ro.security.icd.flagmode=single \
-	persist.radio.apm_sim_not_pwdn=1 \
 	persist.radio.sib16_support=1 \
-	persist.gps.qc_nlp_in_use=1 \
-	persist.loc.nlp_name=com.qualcomm.location \
 	ro.gps.agps_provider=1 \
 	ro.pip.gated=0 \
 	ro.telephony.samsung.realcall=true \
 	cm.updater.uri=http://grandprime.ddns.net/OTA/api \
 	ro.telephony.ril_class=SamsungQcomRIL
 
-# Ril
-PRODUCT_PACKAGES += \
-	libxml2
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Terminal
-PRODUCT_PACKAGES += Terminal
-
-# USB
-PRODUCT_PACKAGES += \
-	com.android.future.usb.accessory
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-	LiveWallpapers \
-	LiveWallpapersPicker \
-	VisualizationWallpapers \
-	librs_jni
-
-# Wifi configuration files
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/wifi/cred.conf:system/etc/wifi/cred.conf \
-	$(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-	$(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-	$(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
-	$(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-	$(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-	$(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-	$(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-	$(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
-	$(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-
-#WLAN
-PRODUCT_PACKAGES += \
-	hostapd \
-	iwconfig \
-	hostapd_cli \
-	libQWiFiSoftApCfg \
-	libqsap_sdk \
-	libwpa_client \
-	libwcnss_qmi \
-	wcnss_service \
-	wpa_supplicant
 
 # Inhert dalvik heap values from aosp
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
