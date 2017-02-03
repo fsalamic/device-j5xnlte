b31adb0a9e613b782b27cf3220c024cc831cb8c5
deadman96385 <seanhoyt963@gmail.com>
2017-02-03 10:48:50 -0600

    Use the msm8916-common ramdisk
    Add init.target.rc for device specfic code

--- device-common.mk
+++ device-common.mk
@@ -79,27 +79,9 @@ ADDITIONAL_BUILD_PROPERTIES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/enableRadioFM:system/etc/init.d/enableRadioFM
 
-# INIT
+# Ramdisk
 PRODUCT_PACKAGES += \
-	fstab.qcom \
-	init.carrier.rc \
-	init.class_main.sh \
-	init.mdm.sh \
-	init.ksm.sh \
-	init.qcom.audio.sh \
-	init.qcom.bt.sh \
-	init.qcom.uicc.sh \
-	init.qcom.wifi.sh \
-	init.qcom.post_boot.sh \
-	init.qcom.class_core.sh \
-	init.qcom.early_boot.sh \
-	init.qcom.syspart_fixup.sh \
-	init.qcom.usb.rc \
-	init.qcom.usb.sh \
-	init.qcom.rc \
-	init.qcom.fm.sh \
-	init.qcom.sh \
-	ueventd.qcom.rc
+	init.target.rc
 
 # Keylayout
 PRODUCT_COPY_FILES += \
