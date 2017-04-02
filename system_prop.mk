7d0a77818100b53f8469b10786fd934b2faf2a1f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-02 04:48:11 -0400

    gprimelte: Merge device props into common

--- system_prop.mk
+++ system_prop.mk
@@ -1,24 +1,12 @@
-# Audio 
-PRODUCT_PROPERTY_OVERRIDES += \
-	qcom.hw.aac.encoder=true
-
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.gps.agps_provider=1 \
-	ro.pip.gated=0
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.cne.feature=0 \
-	persist.radio.apm_sim_not_pwdn=1 \
-	persist.radio.lte_vrte_ltd=1 \
-	persist.radio.add_power_save=1 \
-	persist.radio.sib16_support=1 \
-	rild.libpath=/system/lib/libsec-ril.so \
-	ro.multisim.set_audio_params=true \
-	ro.telephony.samsung.realcall=true \
-	ro.telephony.ril_class=SamsungQcomRIL \
-	ro.use_data_netmgrd=false
+	persist.radio.jbims=1 \
+	persist.radio.no_wait_for_card=1
+
+
+# Telephony
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.default_network=10
 
 # Volume
 PRODUCT_PROPERTY_OVERRIDES += \
