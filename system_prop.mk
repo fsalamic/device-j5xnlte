6a80189eba08d0eb0e47ee446f3ad1597018dbe4
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-12 02:58:50 -0400

    j5: Remove default network system property

--- system_prop.mk
+++ system_prop.mk
@@ -3,11 +3,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.radio.jbims=1 \
 	persist.radio.no_wait_for_card=1
 
-
-# Telephony
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.telephony.default_network=10
-
 # Volume
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=15 \
