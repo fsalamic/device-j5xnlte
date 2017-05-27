9f2b8c75e0e3c623fa73126de54e7902e430b5d1
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-27 03:26:41 -0400

    j5: Inherit vendor blobs

--- device-common.mk
+++ device-common.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from common
 $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
+# Inherit from vendor
+$(call inherit-product-if-exists, vendor/samsung/j5-common/j5-common-vendor.mk)
+
 LOCAL_PATH := device/samsung/j5-common
 
 # System properties
