e1f3acd565e74d6a41c9504d0552476a923ecb4e
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-29 19:37:21 -0400

    j5: Remove enhanced nfc include

--- lineage.mk
+++ lineage.mk
@@ -5,9 +5,6 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy J5
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
