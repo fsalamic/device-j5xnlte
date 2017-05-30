72f68e384a9acca39f257edf0b3313ab30580c0e
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-29 20:02:09 -0400

    j5: Add omni makefile

---
+++ omni.mk
@@ -0,0 +1,19 @@
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy J5
+CM_BUILDTYPE := NIGHTLY
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
