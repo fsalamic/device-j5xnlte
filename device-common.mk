c1cb4ae10b0dbd19b2c2d36ec287704d8c6aad7d
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-31 21:44:45 -0400

    j5: Remove unnecessary name OTA scripts
     * The device name script is no longer necessary,
       since the variant name is now set by libinit_msm8916.
    
       The baseband script is unneeded since baseband is not
       packaged with the rom image.
    
    Change-Id: Ib2b5200be74cc7820ff59d4f2d52283274c29511

--- device-common.mk
+++ device-common.mk
@@ -50,9 +50,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/releasetools/variant_hook.sh:install/bin/variant_hook.sh \
 	$(LOCAL_PATH)/releasetools/installend/copy_nfc_configs.sh:install/bin/installend/copy_nfc_configs.sh \
-	$(LOCAL_PATH)/releasetools/installend/copy_variant_blobs.sh:install/bin/installend/copy_variant_blobs.sh \
-	$(LOCAL_PATH)/releasetools/installend/update_baseband.sh:install/bin/installend/update_baseband.sh \
-	$(LOCAL_PATH)/releasetools/installend/update_device_name.sh:install/bin/installend/update_device_name.sh
+	$(LOCAL_PATH)/releasetools/installend/copy_variant_blobs.sh:install/bin/installend/copy_variant_blobs.sh
 
 # Permissions
 PRODUCT_COPY_FILES += \
