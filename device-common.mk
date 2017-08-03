e1824bf4fcd0015050827259d1bbf1d9606299c0
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-03 09:20:58 -0400

    j5: Remove nfc sec-hal config files
     * The j5 devices (with nfc chips) all use the pn54x hal.
    
    Change-Id: Id974f8b58956be17f3d3eb0cdcb13de111b3458a

--- device-common.mk
+++ device-common.mk
@@ -42,9 +42,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
 	$(LOCAL_PATH)/configs/nfc/route.xml:system/etc/param/route.xml \
 	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf
+	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 # OTA scripts
 PRODUCT_COPY_FILES += \
