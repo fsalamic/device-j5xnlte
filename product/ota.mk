17533ffd88836ed9d2203b37b4c8e401516b656e
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-05 21:07:31 -0400

    j5: Remove OTA scripts
     * These have never been used on any j5 device
    
    Change-Id: I1b848377f6d69850ae07aad578fb66d88dca23c0

--- product/ota.mk
+++
@@ -1,4 +0,0 @@
-# OTA scripts
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/releasetools/variant_hook.sh:install/bin/variant_hook.sh \
-	$(LOCAL_PATH)/releasetools/installend/copy_variant_blobs.sh:install/bin/installend/copy_variant_blobs.sh
