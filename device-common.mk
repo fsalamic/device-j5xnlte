e630d14aedc6a58fa6fb073149815f1c3d199458
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-04 00:17:20 -0400

    j5-common: Move Lineage specific overlays to overlay-lineage
    Change-Id: I9fb18ffb8c7954de214845176b018f04800fd969

--- device-common.mk
+++ device-common.mk
@@ -24,7 +24,9 @@ $(call inherit-product-if-exists, vendor/samsung/j5-common/j5-common-vendor.mk)
 LOCAL_PATH := device/samsung/j5-common
 
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/overlay \
+	$(LOCAL_PATH)/overlay-lineage
 
 # Include package config fragments
 include $(LOCAL_PATH)/product/*.mk
