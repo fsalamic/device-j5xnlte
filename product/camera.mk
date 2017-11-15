9042ac1b337102308432aa6605a425e9b6128de0
Dyneteve <sound0020@gmail.com>
2017-11-15 12:25:10 +0000

    product: Add media_codecs_performance
    Change-Id: If4966593d1604b97b556cfc112d1619b0a0dcc26

--- product/camera.mk
+++ product/camera.mk
@@ -2,10 +2,6 @@
 PRODUCT_PACKAGES += \
     Snap
 
-# Media (camera) configuration files
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
-
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml
---
+++ product/qcom-media.mk
@@ -0,0 +1,4 @@
+# Media configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
