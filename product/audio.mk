35f37ea8f726944d736bd881bc4423e914d0ff13
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-11-19 19:17:51 -0500

    j5-common: Remove mixer paths
    Change-Id: I6fa7e1fff6ae29d750e9f4e046f5832db065327a

--- product/audio.mk
+++ product/audio.mk
@@ -1,7 +1,3 @@
-# Audio configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
