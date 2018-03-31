fbd0f6d5438a4abc6822d6fe0a10390db2f6526a
Dyneteve <sound0020@gmail.com>
2018-03-31 05:38:02 -0400

    j5x-common: Build Camera2
    Change-Id: I9ce63a4301850838908ed7dc28eb43a4b4070fb7

--- product/camera.mk
+++ product/camera.mk
@@ -1,3 +1,7 @@
+# Camera
+PRODUCT_PACKAGES += \
+    Camera2
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml
