1f6b4028c82dde78144410ccfd1aef6b4938899d
Dyneteve <sound0020@gmail.com>
2018-04-26 06:49:35 -0400

    j5x-common: Move mixer_paths to vendor
    Change-Id: I68bfd96fd1aa46ce2101d1970c306149ae557d81

--- product/audio.mk
+++ product/audio.mk
@@ -1,6 +1,6 @@
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
