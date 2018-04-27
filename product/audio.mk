27fc063ccf54f22698bfce8d43a3e5911d12f149
Dyneteve <sound0020@gmail.com>
2018-04-27 09:54:38 -0400

    j5x-common: Add audio_platform_info
    Change-Id: I840045c1b8998a46349f0ee241e01256e1317234

--- product/audio.mk
+++ product/audio.mk
@@ -1,6 +1,7 @@
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
