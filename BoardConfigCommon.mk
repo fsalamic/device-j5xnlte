600d5b0c90864ad73cc8c5b2f1e535e8fca07e0a
Blue Dogerino <fsalamic16@gmail.com>
2018-05-14 21:43:46 +0200

    Update .mk files with correct path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,7 +16,7 @@
 # Inherit from common
 include device/samsung/msm8916-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/j5xnlte
+LOCAL_PATH := device/samsung/j5x-common
 
 # Include board config fragments
 include $(LOCAL_PATH)/board/*.mk
